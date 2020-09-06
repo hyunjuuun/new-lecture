e4aa5bd0272d473a3754ceda68a676d45fa9cb69
hyunjuuun <hyunjun86120@gmail.com>
2020-09-06 23:15:22 +0900

    add ex1, ex2, ex3

---
+++ java/workspace/JavaPrj/src/ex1/io/Program.java
@@ -0,0 +1,22 @@
+package ex1.io;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		int kor1, kor2, kor3;
+		int total;
+		float avg;
+		
+		kor1 = 50;
+		kor2 = 60;
+		kor3 = 80;
+		
+		total = kor1 + kor2 + kor3;
+		avg = total/3;
+		
+		System.out.println(total);
+		System.out.println(avg);
+	}
+
+}
